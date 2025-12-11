8cb2ac3070190dface109812c39f340434ef6bc2
pallavi31-commits <govindraopallavi@gmail.com>
2025-12-11 16:12:55 +0530

    Auth APIs

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const db_config = require("./configs/db.config")
 const user_model = require('./models/user.model')
 const bcrypt = require('bcryptjs')
 
+app.use(express.json())
+
 //Create an admin user at the  starting of the application 
 //if not already present
 
@@ -51,6 +53,9 @@ async function init(){
         console.log("Error while create admin", err)
     }
 }
+
+//Stich the route to the server
+require("./routes/auth.routes")(app)
 
 //start the server
 app.listen(server_config.PORT, ()=>{
