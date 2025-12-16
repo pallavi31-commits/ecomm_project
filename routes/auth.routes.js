80588e78731fa538125b82eb3636927bd70b795f
pallavi31-commits <govindraopallavi@gmail.com>
2025-12-16 15:52:49 +0530

    login with middlewares

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,5 +8,5 @@ module.exports = (app) =>{
     app.post("/ecomm/api/v1/auth/signup",[authMW.verifySignUpBody], authController.signup)
 
     //route for POST localhost:8888/ecomm/api/v1/auth/signin
-    app.post("/ecomm/api/v1/auth/signin",authController.signin)
+    app.post("/ecomm/api/v1/auth/signin",[authMW.verifySignInBody], authController.signin)
 }
